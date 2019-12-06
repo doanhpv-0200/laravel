b0a217dafb9095ced9cb57a7e8049c1dc9dd94c9
Doanh Pham <pham.van.doanh@sun-asterisk.vn>
2019-12-06 10:14:45 +0700

    Add seeder 0.0.2

---
+++ database/seeds/Seeder_0_0_2.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class Seeder_0_0_2 extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // $this->call(UsersTableSeeder::class);
+    }
+}
